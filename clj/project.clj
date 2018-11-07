db221c199ea18a3c2ab0647392fecb96815c0a02
elpinal <elpinal@users.noreply.github.com>
2018-11-07 19:22:35 +0900

    Bring in Clojure

---
+++ clj/project.clj
@@ -0,0 +1,7 @@
+(defproject modules "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "MIT"
+            :url ""}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [hiccup "1.0.5"]])
---
+++ clj/test/modules/core_test.clj
@@ -0,0 +1,7 @@
+(ns modules.core-test
+  (:require [clojure.test :refer :all]
+            [modules.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
