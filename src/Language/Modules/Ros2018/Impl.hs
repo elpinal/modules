57e79515b4b63a6aef791d82497db206604cce78
elpinal <elpinal@users.noreply.github.com>
2019-06-16 22:27:22 +0900

    Add Ordering signature and Int module
    - `Int` module provides `compare` function.

--- src/Language/Modules/Ros2018/Impl.hs
+++ src/Language/Modules/Ros2018/Impl.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE ImplicitParams #-}
+{-# LANGUAGE OverloadedLists #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 {-# OPTIONS_GHC -Wno-orphans #-}
@@ -14,11 +15,16 @@ import qualified Data.Text as T
 
 import Language.Modules.Ros2018 hiding (Env)
 import Language.Modules.Ros2018.Internal (Builtin(..), BaseType(..), insertValue, emptyEnv, name, Failure, Term, Env)
+import qualified Language.Modules.Ros2018.Internal as I
 import Language.Modules.Ros2018.Position
 
 infixr 2 -->
 (-->) :: SemanticType -> SemanticType -> SemanticType
 ty1 --> ty2 = Function $ fromBody $ Fun ty1 Pure $ fromBody ty2
+
+infixr 2 --&>
+(--&>) :: Quantification f => f SemanticType -> SemanticType -> SemanticType
+ty1 --&> ty2 = Function $ quantify (getAnnotatedKinds ty1) $ Fun (getBody ty1) Pure $ fromBody ty2
 
 int :: SemanticType
 int = BaseType Int
@@ -30,6 +36,9 @@ insertValues xs = foldl f ?env xs
       let ?env = acc in
         insertValue (name s) ty
 
+tvarS :: Int -> SemanticType
+tvarS = SemanticPath . fromVariable . I.variable
+
 instance Builtin SemanticType where
   builtins =
     let ?env = emptyEnv in
@@ -37,7 +46,18 @@ instance Builtin SemanticType where
         [ ("+", int --> int --> int)
         , ("-", int --> int --> int)
         , ("*", int --> int --> int)
+        , ("Ordering", AbstractType ordering)
+        , ("Int", Structure
+            [ (I.label "compare", ordering --&> int --> int --> tvarS 0)
+            ])
         ]
+    where
+      ordering = quantify [I.unannotated I.Base] $ Structure
+            [ (I.label "t", AbstractType $ fromBody $ tvarS 0)
+            , (I.label "lt", tvarS 0)
+            , (I.label "eq", tvarS 0)
+            , (I.label "gt", tvarS 0)
+            ]
 
 translate :: Positional Expr -> Either Failure (Either ElaborateError (Term, AbstractType, Purity))
 translate e = run $ runError $ runError $ evalFresh 0 $ let ?env = builtins in elaborate e
