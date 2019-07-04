6faf5f060536660be4f1d9c390a8ffde3644eb82
elpinal <elpinal@users.noreply.github.com>
2019-07-04 18:22:14 +0900

    Define a type-erasure function

--- src/Language/Modules/Ros2018/Internal/Erased.hs
+++ src/Language/Modules/Ros2018/Internal/Erased.hs
@@ -4,6 +4,7 @@
 
 module Language.Modules.Ros2018.Internal.Erased
   ( Term(..)
+  , erase
   ) where
 
 import qualified Data.Text as T
@@ -11,6 +12,7 @@ import Data.Text.Prettyprint.Doc
 import GHC.Exts
 
 import Language.Modules.Ros2018.Display
+import qualified Language.Modules.Ros2018.Internal as I
 import Language.Modules.Ros2018.Internal (Generated, Variable, Label, Record, Literal)
 
 -- | A type-erased term.
@@ -58,3 +60,20 @@ instance Term (Int -> Doc ann) where
   primitive t n = parensIf (5 <= n) $ "primitive" <+> viaShow t
   unpack Nothing x y n  = let_ [x] y n
   unpack (Just g) x y n = parensIf (4 <= n) $ hsep ["let", prettyDisplay g, "=", x 0, "in", y 0]
+
+erase :: Term t => I.Term -> t
+erase (I.Lit l)             = lit l
+erase (I.Var v)             = var v
+erase (I.GVar g)            = gvar g
+erase (I.Abs _ t)           = abs_ $ erase t
+erase (I.App t1 t2)         = app (erase t1) (erase t2)
+erase (I.TmRecord r)        = tmRecord $ erase <$> r
+erase (I.Proj t l)          = proj (erase t) l
+erase (I.Poly _ t)          = poly $ erase t
+erase (I.Inst t _)          = inst $ erase t
+erase (I.Pack t _ _ _)      = pack $ erase t
+erase (I.Unpack mg t1 _ t2) = unpack mg (erase t1) (erase t2)
+erase (I.If t1 t2 t3)       = if_ (erase t1) (erase t2) (erase t3)
+erase (I.Let ts t)          = let_ (erase <$> ts) (erase t)
+erase I.Fix                 = fix
+erase (I.Primitive txt)     = primitive txt
